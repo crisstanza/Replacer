9a57c5b4081c33940ca303df95a13bbd0da6dd20
Cris Stanza <crisstanza@home>
2016-02-29 21:44:24 -0300

    Full english translation.

---
+++ in/$ENTITY$.template.java
@@ -0,0 +1,24 @@
+package x.y.z;
+
+/**
+ * @author 
+ */
+public final class $ENTITY$ {
+
+$FIELDS$
+	private $FIELD_TYPE$ $FIELD$;
+$$FIELDS$
+
+	private $ENTITY$() {
+	}
+
+$FIELDS$
+	public final $FIELD_TYPE$ get$FIELD_camel$() {
+		return this.$FIELD$;
+	}
+	public final void set$FIELD_camel$(final $FIELD_TYPE$ $FIELD$) {
+		this.$FIELD$ = $FIELD$;
+	}
+$$FIELDS$
+
+}
